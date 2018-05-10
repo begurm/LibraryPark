425215b8028e98c925287aaf9cee1815e3e552d2
parksunghoon <parksunghoon@10.30.6.135>
2018-05-10 14:32:05 +0900

    first commit

---
+++ LibraryPark/WebContent/app/components/application/application.component.ts
@@ -0,0 +1,8 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector : 'lib-application',
+  templateUrl : '<div>test</div>',
+  encapsulation : ViewEncapsulation.None
+})
+export default class Applicationcomponent {}
